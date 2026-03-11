55639ec4ac0d3080406384f40b564bbdbac63fed
mohdnas0001 <mohdnas0001@gmail.com>
2026-03-11 08:35:06 +0100

    feat: fixes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,6 +3,6 @@
  */
 
 
-export const baseUrl = 'http://localhost:8000/v1';
+export const baseUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:8000/v1';
 
 export const authUrl = `${baseUrl}/auth/login`
